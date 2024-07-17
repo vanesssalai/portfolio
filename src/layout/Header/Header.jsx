287d42b1742d9adb1e933588b447bd77e27d971b
Vanessa Lai <vlaiyz2605@gmail.com>
2024-07-17 23:17:32 +0800

    added animation to header click, changed icon

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,15 +1,60 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import './Header.css';
 
-export default function Header () {
+export default function Header() {
+    const [showAnimation, setShowAnimation] = useState(false);
+    const [animationFrame, setAnimationFrame] = useState(0);
+
+    const toggleAnimation = () => {
+        setShowAnimation(true);
+        setAnimationFrame(0);
+        const animationElement = document.querySelector('.animation-text');
+        if (animationElement) {
+            animationElement.style.animation = 'none';
+            animationElement.offsetHeight; 
+            animationElement.style.animation = null;
+        }
+    };
+
+    const animationFrames = [
+        "♪♬┏(⌐■_■)┛",
+        "♬♪┗(⌐■_■)┓"
+    ];
+
+    useEffect(() => {
+        let animationInterval;
+        if (showAnimation) {
+            animationInterval = setInterval(() => {
+                setAnimationFrame((prevFrame) => (prevFrame + 1) % animationFrames.length);
+            }, 500); 
+
+            setTimeout(() => {
+                clearInterval(animationInterval);
+                setShowAnimation(false);
+            }, 8000); 
+        }
+        return () => clearInterval(animationInterval);
+    }, [showAnimation]);
+
     return (
-        <header>
-            <div>
-                <a>About Me</a>
-                <a>Projects</a>
-                <a>Resume</a>
-            </div>
-        </header>
+        <>
+            <header>
+                <div className="name">
+                    <a onClick={toggleAnimation}>Vanessa Lai</a>
+                </div>
+                <div className="header-links">
+                    <a>About Me</a>
+                    <a>Projects</a>
+                    <a>Resume</a>
+                </div>
+            </header>
+            {showAnimation && (
+                <div className="animation-container">
+                    <div className="animation-text">
+                        {animationFrames[animationFrame]}
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
